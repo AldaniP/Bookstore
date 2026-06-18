ad0d4ac5d6b8ac2919ad1cd8d402bc01fd878151
Shafly <5026231036@student.its.ac.id>
2026-06-19 06:34:46 +0700

    Rapihkan teks pada file Banner.tsx

--- frontend/src/pages/home/Banner.tsx
+++ frontend/src/pages/home/Banner.tsx
@@ -11,7 +11,7 @@ const Banner = () => {
         <h1 className="md:text-5xl text-2xl font-medium mb-7">
           New Releases This Week
         </h1>
-        <p className="mb-10">
+        <p className="mb-10 text-justify max-w-md">
           It's time to update your reading list with some of the latest and
           greatest releases in the literary world. From heart-pumping thrillers
           to captivating memoirs, this week's new releases offer something for
